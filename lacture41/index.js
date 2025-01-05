b20075f2fbdf2c679664c2218c91e2d682fe9784
Md. Shadab Alam <149529893+shaddy74@users.noreply.github.com>
2025-01-05 18:25:46 +0000

    comp

--- lacture41/index.js
+++ lacture41/index.js
@@ -13,3 +13,46 @@
 
 // CONDITOINAL STATEMENT
 
+// age = 95;
+// if (age>18){
+//     console.log("Can Vote.")
+// }
+// else{
+//     console.log("Cannot Vote")
+// }
+
+
+// let number = 4;
+
+// if (number == 1){
+//     console.log("A")
+// }
+// else if (number == 2){
+//     console.log("B")
+// }
+// else if(number == 3){
+//     console.log("C")
+// }
+// else if(number == 4){
+//     console.log("D")
+// }
+// else if (number == 5){
+//     console.log("E")
+// }
+// else{
+//     console.log("Enter Valid Number")
+// }
+
+
+let num =3;
+
+switch(num){
+    case 1:console.log('A');
+    break;
+    case 2:console.log('B');
+    break;
+    case 3:console.log('C');
+    break;
+    default :console.log('F');
+    break;
+}
