e04cb9a1fd288f60e3410c7008150e77c28ee1e0
Md. Shadab Alam <149529893+shaddy74@users.noreply.github.com>
2025-01-07 16:53:29 +0000

    updated

--- lacture42/index.js
+++ lacture42/index.js
@@ -1,1 +1,71 @@
-console.log("Shadab_Alam")
+// console.log("Shadab_Alam")
+
+
+// FOR LOOP
+
+// for (let i=1; i<=20; i++){
+//     console.log("Shaddy")
+// }
+
+
+// for (let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// for (let i = 10; i>0; i--){
+//     console.log(i)
+// }
+
+
+// for (let i=1; i<8; i++){
+//     if (i==5){
+//         break;
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+
+// for (let i= 1; i<=10; i++){
+//     if(i==5){
+//         continue;
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+
+// WHILE LOOP
+
+// let i=1;
+// while(i<=10){
+//     console.log("Shadab_alam")
+//     i++;
+// }
+ 
+
+// let i=1;
+// while(i<=50){
+//     if(i==6){
+//         break;
+//     0}
+//     else{
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+
+// let i=1;
+// while(i<5){
+//     if(i=3){
+//         i++;
+//         continue;
+//     }
+//     else{
+//         console.log(i)
+//     }
+//     i++;
+// }
