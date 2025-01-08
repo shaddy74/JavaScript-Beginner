0acd65d17ba93c5bd0086318b5bcc1f7a2c4d9b4
Md. Shadab Alam <149529893+shaddy74@users.noreply.github.com>
2025-01-08 08:52:09 +0000

    updated

---
+++ lacture43/index.js
@@ -0,0 +1,18 @@
+// let obj = {
+//     name: "Shadab",
+//     age: 25,
+//     weight: 85,
+//     Height: "6.3 feet",
+//     greet: function(){
+//         console.log("Hello Jee Kaise Ho Saare");
+//     }
+// };
+
+// console.log(obj)
+// obj.greet();
+// console.log(typeof(obj.greet))
+
+// ARRAY
+
+let arr = [1,2,3,4,5,6]
+console.log(arr)
