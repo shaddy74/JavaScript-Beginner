d5feb214c66ecff4181ba065df58c885139b9115
Md. Shadab Alam <149529893+shaddy74@users.noreply.github.com>
2025-01-17 18:26:56 +0000

    function calls

---
+++ lacture45/index.js
@@ -0,0 +1,70 @@
+// // // // sayMyName("Shaddy")
+
+// // // // function sayMyName(finalName) {
+// // // //     console.log(finalName)
+// // // // }
+
+
+// // // // let myGreet = function(){
+// // // //     console.log("Greeting for the day")
+// // // // }
+
+// // // // myGreet()
+
+
+// // // // function greetMe(greet, fullName){
+// // // //     console.log("Hello", fullName);
+// // // //     greet();
+// // // // }
+
+// // // // function greet(){
+// // // //     console.log("greet in the morning")
+// // // // }
+// // // // greetMe(greet, "shaddy");
+
+
+// // // function solve(number) {
+// // //     return function(number){
+// // //         return number*number;
+// // //     }
+// // // }
+
+// // // let ans = solve(5)
+
+// // // let finalAns = ans(10)
+// // // console.log(finalAns)
+
+
+// // const arr = [
+// //     function(a,b){
+// //         return a+b;
+// //     },
+// //     function(a,b){
+// //         return a-b;
+// //     },
+// //     function(a,b){
+// //         return a*b;
+// //     },
+// //     function(a,b){
+// //         return a/b;
+// //     }
+// // ]
+
+// // let first = arr[0];
+// // let ans = first(5,10)
+// // console.log(ans)
+
+
+// let obj = {
+//     age:20,
+//     wt:56,
+//     ht:190,
+//     greet: ()=>{
+//         console.log("Hello Duniya")
+//     }
+// }
+
+// console.log(obj.age)
+// obj.greet()
+
+
