66eb0aa84da56c26632b6d2fc8a76a7d4da7157a
Md. Shadab Alam <149529893+shaddy74@users.noreply.github.com>
2025-01-19 20:22:53 +0000

    math objects & Date-Time objects

---
+++ lacture48/index.js
@@ -0,0 +1,26 @@
+console.log(Math.PI)
+
+console.log(Math.max(21,3,5,5635,665,654,64,64,56,45654,35,35))
+
+console.log(Math.min(21,3,5,5635,665,654,64,64,56,45654,35,35))
+
+console.log(Math.round(21.6))
+
+console.log(Math.ceil(4.1))
+
+console.log(Math.abs(-9))
+
+console.log(Math.random())
+
+
+let curr =  new Date()
+
+let date = new Date("june 20 1992 08:12");
+
+let newDate = new Date(1998, 5, 28, 2);
+
+console.log(newDate);
+
+console.log(newDate.setFullYear(2005));
+
+console.log(newDate);
