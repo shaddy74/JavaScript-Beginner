5f3b558ddcbed52d300dddbe6a2661b0bf116a98
Md. Shadab Alam <149529893+shaddy74@users.noreply.github.com>
2025-01-22 11:45:20 +0000

    try catch & finally block, and throw

--- lacture50/index.js
+++ lacture50/index.js
@@ -1,19 +1,34 @@
-try{
-    console.log("try block starts here");
-    console.log(x);
-    console.log("try block end here");
-}
-catch(e){
-    //define karte h, error ke sath aap kya krna chahte h
-    // retry logic
-    // fallback mechanism
-    // logging
-    // custom error
-    console.log("I AM INSIDE CATCH BLOCK")
-    console.log("Your error is here: ", e);
-}
+// try{
+//     console.log("try block starts here");
+//     console.log(x);
+//     console.log("try block end here");
+// }
+// catch(e){
+//     //define karte h, error ke sath aap kya krna chahte h
+//     // retry logic
+//     // fallback mechanism
+//     // logging
+//     // custom error
+//     console.log("I AM INSIDE CATCH BLOCK")
+//     console.log("Your error is here: ", e);
+// }
 
 
-finally{
-    onslotchange.log("I Will Run everytime , as I am finally block")
+// finally{
+//     onslotchange.log("I Will Run everytime , as I am finally block")
+// }
+
+
+//  lets create a custom error 
+
+// try{
+//     console.log(x);
+// }
+// catch(e){
+//     throw new Error ("Bhai pahle Declare Kro tab code Execute hoga")
+// }
+
+let errorCode = 100;
+if (errorCode == 100){
+    throw new Error ("Invalid Json")
 }
